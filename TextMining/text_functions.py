54d6266f8857212dab8e3a2fffe60d60128c6857
Oscar Felipe Fernandez <67080028+felipefe20@users.noreply.github.com>
2022-12-28 10:05:33 -0500

    Update text_functions.py

--- TextMining/text_functions.py
+++ TextMining/text_functions.py
@@ -1,9 +1,9 @@
         
 def remove_stop_words(table, column, lematize=False):
     import nltk
-    nltk.download(‘stopwords’)
+    nltk.download("stopwords")
     from nltk.corpus import stopwords
-    nltk.download(‘punkt’)
+    nltk.download("punkt")
     from nltk.tokenize import word_tokenize
     import regex as re
 
